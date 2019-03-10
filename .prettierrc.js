e9799cb305bef6ef780699fffd830ffd3c841d11
Mextli <luna.vazquezv@gmail.com>
2019-03-10 01:52:10 -0600

    configuracion del proyecto

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  jsxBracketSameLine: true,
+  bracketSpacing: true,
+  jsxSingleQoute: true,
+  trailingComma: "all",
+  singleQuote: true,
+  printWidth: 100,
+  useTabs: false,
+  semi: true
+};
