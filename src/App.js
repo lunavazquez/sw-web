9b2f34bf74e8a60a8f1e5f51c355c97b58db57d6
Mextli <luna.vazquezv@gmail.com>
2019-03-12 00:15:21 -0600

    se agrega history para las rutas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // mantener la app en una sola pagina
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Router, Switch, Route } from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory';
 import Home from './pages/home';
 import Starships from './pages/starships';
 import background from './assets/124129.jpg';
@@ -9,7 +10,7 @@ import styles from './App.sass';
 const App = () => {
   return (
     <div className={styles.App} style={{ backgroundImage: `url(${background})` }}>
-      <Router>
+      <Router history={createBrowserHistory()}>
         <Switch>
           {/* decide que componente va renderisar por la ruta  */}
           <Route exact path="/" component={Home} />
