5661f81c4ffec2de1043558505a2048cf75c38cb
Mextli <luna.vazquezv@gmail.com>
2019-03-11 23:54:11 -0600

    Updates

---
+++ precache-manifest.c2236c30e56b39f54d6e4ff5e2a30cc6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "32c99cf66f769d06cfa0a7da8277aa17",
+    "url": "/sw-web/static/media/3.32c99cf6.jpg"
+  },
+  {
+    "revision": "a0b5e7e0332d9f26dc68",
+    "url": "/sw-web/static/css/main.d54dd099.chunk.css"
+  },
+  {
+    "revision": "4932363962c140d41c4c",
+    "url": "/sw-web/static/js/runtime~main.1e24cc0f.js"
+  },
+  {
+    "revision": "0914ebbb6dcfc7186524",
+    "url": "/sw-web/static/js/2.ed5c35f8.chunk.js"
+  },
+  {
+    "revision": "0f3d0156c0015cf21466997fd563e62c",
+    "url": "/sw-web/static/media/1.0f3d0156.jpg"
+  },
+  {
+    "revision": "19f2d33d1b9a8a11c3dc29e02c464fe0",
+    "url": "/sw-web/static/media/2.19f2d33d.jpg"
+  },
+  {
+    "revision": "a0b5e7e0332d9f26dc68",
+    "url": "/sw-web/static/js/main.be510657.chunk.js"
+  },
+  {
+    "revision": "f01b97ffc74797d46c6a4c5c38daede4",
+    "url": "/sw-web/static/media/4.f01b97ff.jpg"
+  },
+  {
+    "revision": "0cbc0e3c43b4e3c0bc6e56b162ef793c",
+    "url": "/sw-web/static/media/5.0cbc0e3c.jpg"
+  },
+  {
+    "revision": "c41d1fb5768c3c5b0db640de8a65a8a0",
+    "url": "/sw-web/static/media/6.c41d1fb5.jpg"
+  },
+  {
+    "revision": "bc07c078819e43c81a2c3202361ffc2e",
+    "url": "/sw-web/static/media/7.bc07c078.jpg"
+  },
+  {
+    "revision": "2404a29efa5e3130a0d1764ca69e2888",
+    "url": "/sw-web/static/media/124129.2404a29e.jpg"
+  },
+  {
+    "revision": "dec6f2a7abd5302704849134e1506e15",
+    "url": "/sw-web/index.html"
+  }
+];
